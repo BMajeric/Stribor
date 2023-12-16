b82b57da3a495191d63b1c9dcb1153d984a620e4
VlasnikTvorniceOraha <jf53287@fer.hr>
2023-12-16 20:38:36 +0100

    Bacanje svarozica

--- Stribor/Assets/Scripts/SvarozicGaming.cs
+++ Stribor/Assets/Scripts/SvarozicGaming.cs
@@ -12,9 +12,11 @@ public class SvarozicGaming : MonoBehaviour
     public KeyCode BaciSvarozicaGumb = KeyCode.Mouse0;
 
     //koliko ih se moze bacati
-    public int BrojSvarozica = 2;
+    public int BrojSvarozica;
 
     public GameObject SvarozicPrefab;
+
+    public Transform SvaroziciParent;
 
     //igrac ce moci ici do kamina da ojaca svijetlo svarozica
     public int snagaSvarozica = 1;
@@ -22,17 +24,54 @@ public class SvarozicGaming : MonoBehaviour
     public TMP_Text SvaroziciTekst;
 
     //lokacija s kojeg ce bacat svarozice
-    public Transform startLokacija;
+    public Vector3 startLokacija;
+
+    //Snaga lansiranja
+    public float LaunchForce;
+
+    Vector3 smjerLansiranja;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Pronadi zoom object igraca (eye level)
+        startLokacija = GameObject.FindGameObjectWithTag("Player").transform.Find("ZoomObject").transform.position;
+
+        LaunchForce = 40f;
+
+        SvaroziciTekst.text = "Svarozici: " + BrojSvarozica;
+
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(BaciSvarozicaGumb) && BrojSvarozica > 1) {
+            LansirajSvarozica();
+        }
+
         
+    }
+
+    void LansirajSvarozica() {
+        Debug.Log("Lansiroje");
+
+        startLokacija = GameObject.FindGameObjectWithTag("Player").transform.Find("SvarozicURuci").transform.position;
+
+        GameObject SvarozicInWorld = Instantiate(SvarozicPrefab, SvaroziciParent);
+
+        SvarozicInWorld.transform.position = startLokacija;
+
+        smjerLansiranja = Camera.main.transform.forward;
+
+        SvarozicInWorld.GetComponent<Rigidbody>().AddForce(smjerLansiranja * LaunchForce);
+
+        BrojSvarozica -= 1;
+
+        SvaroziciTekst.text = "Svarozici: " + BrojSvarozica;
+
     }
 }
